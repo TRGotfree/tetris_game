cb343b65dfef33dc174d7eb71a835ac07df6da8f
timur <timur.gazaleyev@gmail.com>
2020-10-29 17:38:56 +0500

    controllers added

---
+++ backend/tetris/src/controllers/signup.controller.ts
@@ -0,0 +1,8 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('signup')
+export class SignUpController {
+    constructor() {
+        
+    }
+}
--- clientapp/tetris/src/app/app.module.ts
+++ clientapp/tetris/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      LoginComponent,
+      SignupComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
